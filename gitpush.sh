3ddc076d04acc608fc262ababad464c426d4961f
Fiko Aditama <fiko@simise.id>
2024-05-23 14:30:59 +0700

    navbar

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "first commit"
+git commit -m "navbar"
 git branch -M main
 git remote add origin https://github.com/fico-aditama/welcome-home.git
 git push -u origin main
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./app.scss"
+import Navbar from "../components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar/>
+    </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portofolio1</section>
+    <section>Portofolio2</section>
+    <section>Portofolio3</section>
+    <section>Contact</section>
+
+  </div>;
 };
 
 export default App;
